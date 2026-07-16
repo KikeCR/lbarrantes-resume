ef64e95b1e035f620c4fb3620baaeee98127c75f
KikeCR <luis.barrantesv@gmail.com>
2026-07-16 08:20:52 -0600

    Navbar and ContactMeForm migrated to TSX and Tailwind.

---
+++ src/Navbar.tsx
@@ -0,0 +1,54 @@
+import { useContext } from 'react';
+import { AppBar, Toolbar, IconButton, FormControl, MenuItem, Select } from '@mui/material';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { Fade } from 'react-awesome-reveal';
+
+import { LanguageContext } from './contexts/language.context';
+import { ThemeContext } from './contexts/theme.context';
+
+function Navbar() {
+	const { isDarkMode, toggleTheme } = useContext(ThemeContext);
+	const { language, changeLanguage } = useContext(LanguageContext);
+
+	return (
+		<AppBar
+			position="fixed"
+			sx={{
+				'&.MuiAppBar-colorPrimary.MuiPaper-root': {
+					backgroundColor: 'var(--color-main)',
+					color: 'var(--color-navbar-title-text)',
+				},
+				'& .MuiToolbar-regular': { minHeight: 'var(--spacing-navbar-height)' },
+			}}
+		>
+			<Toolbar>
+				<Fade duration={2000} triggerOnce>
+					<div className="px-5 text-lg font-bold tracking-wide">
+						lbarrantes<span className="text-secondary">.</span>
+					</div>
+				</Fade>
+				<div className="order-2 ml-auto flex">
+					<FormControl variant="outlined">
+						<Select
+							value={language}
+							onChange={changeLanguage}
+							sx={{
+								'&, & .MuiSelect-iconOutlined': { color: 'var(--color-navbar-icons)' },
+								'& .MuiOutlinedInput-notchedOutline': { borderColor: 'transparent' },
+							}}
+						>
+							<MenuItem value="en">EN</MenuItem>
+							<MenuItem value="es">ES</MenuItem>
+						</Select>
+					</FormControl>
+					<IconButton edge="end" color="inherit" aria-label="menu" onClick={toggleTheme}>
+						{isDarkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+					</IconButton>
+				</div>
+			</Toolbar>
+		</AppBar>
+	);
+}
+
+export default Navbar;
